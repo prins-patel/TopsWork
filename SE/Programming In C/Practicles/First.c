1da3dd58304daa207c4d7e82f6984a829fcc1547
prins-patel <prinspatel.dev@gmail.com>
2026-05-13 13:56:20 +0530

    Add files via upload

---
+++ SE/Programming In C/Practicles/First.c
@@ -0,0 +1,11 @@
+/* 
+	This is my first practicle of Programming C.
+*/
+
+#include<stdio.h>
+int main()
+{
+	
+	printf("Hello World!"); //this printf function used to Print statement on output screen
+	printf("\nI am \"Prins Patel\".");
+}
